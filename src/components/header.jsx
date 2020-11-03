f8d0c0e5984845ad657490de686c75722036372f
Pastelhorizon <42344969+Pastelhorizon@users.noreply.github.com>
2020-11-03 12:17:32 +0530

    Initial commit of Keeper App

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return <header>
+        <h1>Keeper</h1>
+        <br />
+        <p>It doesn't keep you data !!</p>
+    </header>
+}
+
+export default Header;
---
+++ src/components/note.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Note(){
+    return <div className = "note">
+    <h1>A sample note heading</h1>
+    <p>A sample note paragraph</p>
+    </div>;
+}
+
+export default Note;
