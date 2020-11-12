813eb9962d4e48351282b05fd78d8da9e1d3da53
Pastelhorizon <42344969+Pastelhorizon@users.noreply.github.com>
2020-11-12 10:40:42 +0530

    functionality of adding and deleting notes from an array is added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./header"
 import Footer from "./footer"
 import NoteComponent from "./note-compopnent"
-import Notes from "../notes"
-console.log(Notes);
+//import Notes from "../notes"
+import CreateArea from "./CreateArea"
+
+//console.log(Notes);
+
+
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNewNote(newNote){
+        console.log(newNote);
+        setNotes(function(prev){
+            return [...prev, newNote]
+        })
+    }
+
+    function deleteItem(id){
+        setNotes(function(prev){
+            return (
+                prev.filter(function(noteItem, noteId){
+                    return noteId !== id;
+                })
+            )
+        })
+    }
+
     return <div>
         <Header />
+        <CreateArea onAdd={addNewNote} />
 
-
-        {Notes.map(function(eachNote){
+        {notes.map(function(eachNote, index){
             return( 
             <NoteComponent 
-            key={eachNote.key}
+            id={index}
+            key={index}
             title={eachNote.title}
             content={eachNote.content}
+            onDelete={deleteItem}
             />
             );
         })}
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,40 @@
+
+import { useState } from "react";
+
+function CreateArea(props){
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+    function handleChange(event){
+        const {name, value} = event.target;
+        setNote(function(prevNote){
+            return {
+                ...prevNote,
+                [name] : value
+            };
+        });
+    }
+    function handleClick(event){
+        event.preventDefault();
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        });
+    }
+        
+    
+
+    return (
+        <div>
+            <form>
+                <input name="title" placeholder="Title"  value={note.title} onChange={handleChange}/>
+                <textarea name="content" placeholder="Take a note..." rows="3"  value={note.content} onChange={handleChange}/>
+                <button onClick={handleClick}>+</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
