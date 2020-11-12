ac1d64df2cbc051f71873f48aa56dece57e2fcb2
Pastelhorizon <42344969+Pastelhorizon@users.noreply.github.com>
2020-11-13 01:53:44 +0530

    Changed color of delete icon

--- src/components/note-compopnent.jsx
+++ src/components/note-compopnent.jsx
@@ -11,7 +11,7 @@ function Note(props){
     <div className = "note">
     <h1>{props.title}</h1>
     <p>{props.content}</p>
-    <IconButton onClick={handleClick} ><DeleteIcon /></IconButton>
+    <IconButton onClick={handleClick} ><DeleteIcon style={{color: "#f74242"}} /></IconButton>
     </div>
     );
 }
